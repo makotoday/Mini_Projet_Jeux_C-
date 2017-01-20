4507d75c8f549d82755a5128ef9dceca0533052f
seblast31 <sebastien.martin1@u-psud.fr>
2017-01-20 18:19:24 +0100

    affichage dans fichier texte

--- jeux.cpp
+++ jeux.cpp
@@ -1,10 +1,28 @@
 #include "CAireJeux.h"
+#include <fstream>
+#include <string.h>
+
+using namespace std;
+
+ofstream monFichier; //varialbe globale
+
+void EcrireDans(CAireJeux& jeux,string nomF);
 
 int main(){
 
 
-	CAireJeux jeux;
+    cout << "L'Affichage est dans resultat.txt"<< endl;
+    // Redirection de std::cout vers le fichier resultat.txt
+    ofstream Out("resultat.txt");
+    streambuf* OldBuf = cout.rdbuf(Out.rdbuf());
+
+    CAireJeux jeux;
 	srand(time(NULL));
-	jeux.Run();
+    jeux.Run();
 	cout<<"\n------------------FIN DE JEUX -----------------\n";
+
+    // Restauration du streambuf initial de std::cout (affichage sur la console)
+    std::cout.rdbuf(OldBuf);
+
+
 }
