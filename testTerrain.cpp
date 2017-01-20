d7b954b0120631208fbdeb75963c304d18124309
seblast31 <sebastien.martin1@u-psud.fr>
2017-01-20 13:14:16 +0100

    commit v5

--- testTerrain.cpp
+++ testTerrain.cpp
@@ -1,25 +1,25 @@
 #include "CAireJeux.h"
 #include <iostream>
-using namespace std; 
+using namespace std;
 /*ce ficher, est le fichier test de la classe CAireJeux afin de tester toute ses methode  */
 
 int main(){
 
 
-	CAireJeux jeux; 
-	jeux.printT(); 
-	cout<<"Modifier une case en donnant sont indice :  ";
-	int t_case; 
-	int element_case; 
-	cin>>t_case; 
-	cout<<"entrer un chiffre soit 3  ou 2 : "; 
-	cin>>element_case; 
-	jeux.modifieCase(t_case,element_case); 
+	CAireJeux jeux;
+	jeux.printT();
+	cout<<"Modifier une case en donnant son indice :  ";
+	int t_case;
+	int element_case;
+	cin>>t_case;
+	cout<<"entrer un chiffre soit 3  ou 2 : ";
+	cin>>element_case;
+	jeux.modifieCase(t_case,element_case);
 	jeux.printT();
 
 
 
 
 
-return 1; 
+return 1;
 }
